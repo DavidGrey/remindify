dc1e121a3cc7fdadb9b9fe9a1ae12668586c92c7
APerson <danielhglus@gmail.com>
2015-03-28 12:05:47 -0400

    adding calendar

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/addevent")
+def addevent():
+    return render_template("addevent.html")
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
