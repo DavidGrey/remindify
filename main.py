7cb82df6291fa7c7c5b020c06aadf24bee8a476a
APerson <danielhglus@gmail.com>
2015-03-28 13:03:27 -0400

    fixing addevent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/addevent")
-def addevent():
-    return render_template("addevent.html")
+@app.route("/addevent", methods=["GET", "POST"])
+def addevent_submit():
+    if request.method == "GET":
+        return render_template("addevent.html")
+    else:
+        return "Submitted event called '{}'.".format(request.form["name"])
 
 if __name__ == "__main__":
     app.debug = True
