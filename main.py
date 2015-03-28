eb1bd4536f6ae71362e1ea9071298e0cb5c548b2
APerson <danielhglus@gmail.com>
2015-03-28 15:03:32 -0400

    added writing data from addevent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +12,23 @@ def addevent_submit():
     if request.method == "GET":
         return render_template("addevent.html")
     else:
+
+        # Write data
+        event = dict()
+        event["name"] = request.form["name"]
+        event["location"] = request.form["location"]
+        event["datetime"] = request.form["date"] + " " + request.form["time"]
+        event["email"] = request.form["email"]
+        event["message"] = request.form["message"]
+        
+        data = dict()
+        with open("schedule.json", "r+") as schedule:
+            data = json.load(schedule)
+            data += [event]
+
+        with open("schedule.json", "w") as schedule:
+            json.dump(data, schedule)
+            
         return render_template("submitevent.html", name=request.form["name"])
 
 if __name__ == "__main__":
